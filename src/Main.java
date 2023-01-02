8d2c6d9ba882a3f56f7cbe65e334345755945fdf
RomanKamko <r.kamko@mail.ru>
2023-01-02 13:02:09 +0400

    Объявил константу которую можно использовать в других методах Решил задачку поиска максимума из трех чисел через if

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+    public static final double CM_PER_INCH = 2.54;
+
+    // константа которую можно использовать в других иетодах
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+
+        int one = 10;
+        int two = 10;
+        int three = 9;
+        System.out.println();
+        if (one > two) {
+            if (one>three) {
+                System.out.println(one);
+            }else{
+                System.out.println(three);
+            }
+        } else if (two>three) {
+            System.out.println(two);
+        } else {
+            System.out.println(three);
+        }
+
+
     }
+
 }
