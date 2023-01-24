4eab239047ffee12e0c06096ceaeb847702fc5cd
RomanKamko <r.kamko@mail.ru>
2023-01-24 19:26:56 +0400

    строки

---
+++ src/LessonArrays.java
@@ -0,0 +1,34 @@
+public class LessonArrays {
+    public static void main(String[] args) {
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        int[] weightsCopy = {902, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+
+        boolean arraysAreEqual = true;
+        arraysAreEqual = weights.length == weightsCopy.length;
+        if (arraysAreEqual) {
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] != weightsCopy[i]) {
+                    arraysAreEqual = false;
+                }
+            }
+        }
+
+        if (arraysAreEqual) {
+            System.out.println("Массивы равны");
+        } else {
+            System.out.println("Массивы не равны");
+        }
+
+        int maxWeight = -1;
+        for (int current : weights) {
+            if (current > maxWeight) {
+                maxWeight = current;
+            }
+        }
+        System.out.println(maxWeight);
+
+        for (int i = 0; i < weights.length - 1 && weights[i + 1] != 0; i++) {
+            System.out.println(weights[i + 1] - weights[i]);
+        }
+    }
+}
